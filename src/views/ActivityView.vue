ab1afa3f0852dae79c22fb0ad0e2e2f4094f920a
Mélanie Couronne <melanie.couronne@gmail.com>
2024-08-31 15:18:59 +0200

    init projet

---
+++ src/views/ActivityView.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id="container">
+    <div
+      v-for="(square, index) in squares"
+      :key="index"
+      class="activity"
+      @mouseover="addColor"
+    ></div>
+  </div>
+</template>
+
+<script setup>
+  import { ref, onMounted } from "vue";
+
+  const greenColorsHexa = ref(["#9be9a8", "#40c463", "#30a14e", "#216e39"]);
+  const squares = ref([]);
+
+  const addColor = (event) => {
+    const color = greenColors();
+    event.target.style.backgroundColor = color;
+  };
+
+  const greenColors = () => {
+    return greenColorsHexa.value[
+      Math.floor(Math.random() * greenColorsHexa.value.length)
+    ];
+  };
+
+  const calculateNumberOfSquares = () => {
+    const containerWidth = window.innerWidth - 20;
+    const containerHeight = window.innerHeight - 20;
+    const squareSize = 12;
+    const numberOfSquares =
+      Math.ceil(containerWidth / squareSize) *
+      Math.ceil(containerHeight / squareSize);
+    return numberOfSquares;
+  };
+
+  onMounted(() => {
+    squares.value = Array(calculateNumberOfSquares()).fill(null);
+  });
+</script>
+
+<style scoped>
+  #container {
+    display: grid;
+    align-items: center;
+    justify-content: center;
+    justify-items: center;
+    grid-template-columns: repeat(auto-fill, 12px);
+    gap: 4px;
+    width: calc(100vw - 10px);
+    height: calc(100vh - 10px);
+    padding: 5px;
+    overflow: hidden;
+  }
+  .activity {
+    background-color: rgb(234, 237, 240);
+    /* box-shadow: 0 0 2px rgb(151, 151, 151); */
+    outline: 1px solid rgba(27, 31, 35, 0.06);
+    outline-offset: -1px;
+    margin: 1px 2px;
+    height: 12px;
+    width: 12px;
+    overflow: hidden;
+    border-radius: 2px;
+  }
+  .activity:hover {
+    transition: background-color 0.3s;
+    overflow: hidden;
+  }
+</style>
