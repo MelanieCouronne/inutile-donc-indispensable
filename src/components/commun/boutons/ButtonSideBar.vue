3dc7b938440db32ee8c85e90d7c787db4c452851
Mélanie Couronne <melanie.couronne@gmail.com>
2024-09-03 10:36:36 +0200

    Ajout d'un bouton composant pour la sideBar

---
+++ src/components/commun/boutons/ButtonSideBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    @click="emitOnClick"
+    class="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-teal-500 hover:text-white rounded-md transition duration-150 ease-in-out cursor-pointer"
+  >
+    <svg
+      class="w-6 h-6 fill-current inline-block"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path :d="props.svgPath"></path>
+    </svg>
+    <span>&nbsp;{{ props.title }}</span>
+  </div>
+</template>
+
+<script setup>
+  const props = defineProps({
+    svgPath: { String, required: true },
+    title: { String, required: true },
+    componentName: { String, required: true },
+  });
+
+  const emit = defineEmits(["buttonClicked"]);
+
+  const emitOnClick = () => {
+    emit("buttonClicked", props.componentName);
+  };
+</script>
--- src/components/commun/navigation/sideBar.vue
+++ src/components/commun/navigation/sideBar.vue
@@ -73,42 +73,28 @@
             </button>
           </div>
           <div id="menu" class="flex flex-col space-y-2">
-            <div
-              @click="selectComponent('ActivitySquares')"
-              class="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-teal-500 hover:text-white hover:text-base rounded-md transition duration-150 ease-in-out"
-            >
-              <svg
-                class="w-6 h-6 fill-current inline-block"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"
-                ></path>
-              </svg>
-              <span class="">Activité</span>
-            </div>
+            <ButtonSideBar
+              @buttonClicked="selectComponent"
+              svgPath="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"
+              title="Activité (vs carrés)"
+              componentName="ActivitySquares"
+            />
 
-            <div
-              @click="selectComponent('ActivityRods')"
-              href=""
-              class="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-teal-500 hover:text-white hover:text-base rounded-md transition duration-150 ease-in-out"
-            >
-              <svg
-                class="w-6 h-6 fill-current inline-block"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"
-                ></path>
-              </svg>
-              <span class="">L'effet Jean-Michel Jarre</span>
-            </div>
+            <ButtonSideBar
+              @buttonClicked="selectComponent"
+              svgPath="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"
+              title="Activité (vs barres)"
+              componentName="ActivityRods"
+            />
 
-            <a
+            <ButtonSideBar
+              @buttonClicked="selectComponent"
+              svgPath="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"
+              title="Composant fort peu utile"
+              componentName="CatchMeIfYouCan"
+            />
+
+            <!-- <a
               href=""
               class="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-teal-500 hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out"
             >
@@ -177,7 +163,7 @@
                 ></path>
               </svg>
               <span class="">UI Components</span>
-            </a>
+            </a> -->
           </div>
         </div>
       </div>
@@ -187,6 +173,7 @@
 
 <script setup>
   import { computed, inject, onMounted, onUnmounted, ref } from "vue";
+  import ButtonSideBar from "@/components/commun/boutons/ButtonSideBar.vue";
 
   const activeComponent = inject("activeComponent");
 
