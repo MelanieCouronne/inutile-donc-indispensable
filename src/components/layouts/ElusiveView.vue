c5d81903054085031ea59335258325c209b1e0a0
Mélanie Couronne <melanie.couronne@gmail.com>
2024-09-03 12:55:53 +0200

    Nouvelle approche du runAway

--- src/components/layouts/ElusiveView.vue
+++ src/components/layouts/ElusiveView.vue
@@ -1,9 +1,9 @@
 <template>
-  <div id="main-container" ref="containerDimensions">
+  <div id="main-container" ref="containerDimensions" @mousemove="runAway">
     <button
-      ref="buttonDimensions"
+      ref="buttonRunAway"
       :style="{ left: `${position.x}px`, top: `${position.y}px` }"
-      class="absolute text-sm font-medium py-2 px-2 bg-teal-500 text-white rounded-md cursor-pointer"
+      class="absolute text-sm font-medium py-2 px-2 bg-teal-500 text-white rounded-md cursor-pointer transition duration-50 ease-in-out"
     >
       <span>&nbsp; Un titre</span>
     </button>
@@ -11,11 +11,11 @@
 </template>
 
 <script setup>
-  import { ref, onMounted } from "vue";
+  import { ref, onMounted, onUnmounted } from "vue";
 
   const position = ref({ x: 0, y: 0 });
   const containerDimensions = ref(null);
-  const buttonDimensions = ref(null);
+  const buttonRunAway = ref(null);
 
   const getContainerDimensions = () => {
     if (!containerDimensions.value) return { width: 0, height: 0 };
@@ -26,11 +26,11 @@
     };
   };
 
-  const getButtonDimensions = () => {
-    if (!buttonDimensions.value) return { buttonWidth: 0, buttonHeight: 0 };
+  const getbuttonDimension = () => {
+    if (!buttonRunAway.value) return { buttonWidth: 0, buttonHeight: 0 };
 
-    const w = buttonDimensions.value ? buttonDimensions.value.offsetWidth : 0;
-    const h = buttonDimensions.value ? buttonDimensions.value.offsetHeight : 0;
+    const w = buttonRunAway.value ? buttonRunAway.value.offsetWidth : 0;
+    const h = buttonRunAway.value ? buttonRunAway.value.offsetHeight : 0;
 
     return {
       buttonWidth: w,
@@ -40,15 +40,27 @@
 
   const setCenter = () => {
     const { width, height } = getContainerDimensions();
-    const { buttonWidth, buttonHeight } = getButtonDimensions();
+    const { buttonWidth, buttonHeight } = getbuttonDimension();
 
     const x = (width - buttonWidth) / 2;
     const y = (height - buttonHeight) / 2;
     return { x, y };
   };
 
+  const runAway = () => {
+    buttonRunAway.value.style = `transform: translate(${
+      Math.random() * 80 - 40
+    }vw, ${Math.random() * 80 - 40}vh);`;
+    buttonRunAway.value.innerText = "Not way, Jose !";
+  };
+
   onMounted(() => {
     position.value = setCenter();
+    window.addEventListener("mousemove", runAway);
+  });
+
+  onUnmounted(() => {
+    window.removeEventListener("mousemove", runAway);
   });
 </script>
 
