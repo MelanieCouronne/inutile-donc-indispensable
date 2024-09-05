75e0624c74cfe7f159a576b8b995649fd4e698a2
Mélanie Couronne <melanie.couronne@gmail.com>
2024-09-05 12:32:34 +0200

    Creation d'un bouton rond en composant

---
+++ src/components/commun/boutons/ButtonRounded.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    @click="emitOnClick"
+    class="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-teal-500 hover:text-white rounded transition duration-150 ease-in-out cursor-pointer"
+  >
+    <svg
+      class="w-6 h-6 fill-current inline-block"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path :d="props.svgPath"></path>
+    </svg>
+  </div>
+</template>
+
+<script setup>
+  const props = defineProps({
+    svgPath: { String, required: true },
+    actionName: { String, required: true },
+  });
+
+  const emit = defineEmits(["buttonClicked"]);
+
+  const emitOnClick = () => {
+    emit("buttonClicked", props.actionName);
+  };
+</script>
