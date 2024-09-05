3402271e47952ed9c242adffce4e326b2d477595
Mélanie Couronne <melanie.couronne@gmail.com>
2024-09-05 14:26:30 +0200

    Changement de svg dans le notification

--- src/components/commun/NotificationView.vue
+++ src/components/commun/NotificationView.vue
@@ -2,7 +2,7 @@
   <div
     id="notification"
     aria-live="assertive"
-    class="pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6"
+    class="z-50 pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6"
   >
     <div class="flex w-full flex-col items-center space-y-4 sm:items-end">
       <div
@@ -22,7 +22,7 @@
                 <path
                   stroke-linecap="round"
                   stroke-linejoin="round"
-                  d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0M3.124 7.5A8.969 8.969 0 0 1 5.292 3m13.416 0a8.969 8.969 0 0 1 2.168 4.5"
+                  d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
                 />
               </svg>
             </div>
@@ -30,9 +30,6 @@
               <p class="text-sm font-medium text-gray-900">Notification</p>
               <p class="mt-1 text-sm text-gray-500">
                 &#8220;{{ message }}&#8221;
-              </p>
-              <p class="text-xs text-gray-500 italic text-right mr-1">
-                &#8213; JCVD
               </p>
             </div>
             <div class="ml-4 flex flex-shrink-0">
