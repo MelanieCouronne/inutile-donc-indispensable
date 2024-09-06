333af481f3d1e8667174d1ed7ab0df58b2d02ea7
Mélanie Couronne <melanie.couronne@gmail.com>
2024-09-06 16:00:54 +0200

    Route pour 404 NotFound.vue

--- src/router/index.js
+++ src/router/index.js
@@ -53,7 +53,15 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
+      component: () => import("@/views/AboutView.vue"),
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      name: "NotFound",
+      component: () => import("@/components/layouts/NotFound.vue"),
+      // meta: {
+      //   title: `${import.meta.env.VITE_APP_TITLE} - 404 Not Found`,
+      // },
     },
   ],
 });
