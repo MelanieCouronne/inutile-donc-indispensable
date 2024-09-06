6c388c51c56963a79d7d3a55166dc0b15223b919
Mélanie Couronne <melanie.couronne@gmail.com>
2024-09-06 15:52:47 +0200

    Les SVG

--- src/components/navigation/SideBar.vue
+++ src/components/navigation/SideBar.vue
@@ -50,7 +50,6 @@
           </div>
 
           <div id="search">
-            <!-- <p class="text-sm font-medium text-gray-700">Rechercher un JCVD</p> -->
             <div
               class="flex border-2 border-gray-200 rounded-md focus-within:ring-2 ring-teal-500"
             >
@@ -138,26 +137,6 @@
               >
             </button>
           </div>
-          <svg
-            class="w-6 h-6 fill-current inline-block"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM14 11a1 1 0 011 1v1h1a1 1 0 110 2h-1v1a1 1 0 11-2 0v-1h-1a1 1 0 110-2h1v-1a1 1 0 011-1z"
-            ></path>
-          </svg>
-          <svg
-            class="w-6 h-6 fill-current inline-block"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zM15.211 6.276a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z"
-            ></path>
-          </svg>
         </div>
         <FooterLayout />
       </div>
