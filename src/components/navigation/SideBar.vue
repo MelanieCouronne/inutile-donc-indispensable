7cbaecc9fe19aedc41c60f71bc7311a127839c75
Mélanie Couronne <melanie.couronne@gmail.com>
2024-09-10 15:56:24 +0200

    dark mode toggle

--- src/components/navigation/SideBar.vue
+++ src/components/navigation/SideBar.vue
@@ -20,8 +20,48 @@
       </button>
       <div
         v-if="showNavigation"
-        class="relative bg-white dark:bg-slate-800 h-screen w-screen md:w-60 lg:w-60 md:block shadow-xl px-3 pt-20 md:pt-6 w-30 overflow-x-hidden transition-transform duration-300 ease-in-out space-y-6 md:space-y-10"
+        class="relative bg-white dark:bg-slate-800 h-screen w-screen md:w-60 lg:w-60 md:block shadow-xl px-3 pt-20 md:pt-4 w-30 overflow-x-hidden transition-transform duration-300 ease-in-out space-y-6 md:space-y-10"
       >
+        <!-- Dark / Light mode -->
+        <button
+          class="relative cursor-pointer p-2"
+          id="light-switch"
+          @click="toggleTheme"
+        >
+          <svg
+            class="dark:hidden"
+            width="16"
+            height="16"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              class="fill-gray-300"
+              d="M7 0h2v2H7zM12.88 1.637l1.414 1.415-1.415 1.413-1.413-1.414zM14 7h2v2h-2zM12.95 14.433l-1.414-1.413 1.413-1.415 1.415 1.414zM7 14h2v2H7zM2.98 14.364l-1.413-1.415 1.414-1.414 1.414 1.415zM0 7h2v2H0zM3.05 1.706 4.463 3.12 3.05 4.535 1.636 3.12z"
+            />
+            <path
+              class="fill-gray-400"
+              d="M8 4C5.8 4 4 5.8 4 8s1.8 4 4 4 4-1.8 4-4-1.8-4-4-4Z"
+            />
+          </svg>
+          <svg
+            class="hidden dark:block"
+            width="16"
+            height="16"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              class="fill-slate-400"
+              d="M6.2 1C3.2 1.8 1 4.6 1 7.9 1 11.8 4.2 15 8.1 15c3.3 0 6-2.2 6.9-5.2C9.7 11.2 4.8 6.3 6.2 1Z"
+            />
+            <path
+              class="fill-slate-500"
+              d="M12.5 5a.625.625 0 0 1-.625-.625 1.252 1.252 0 0 0-1.25-1.25.625.625 0 1 1 0-1.25 1.252 1.252 0 0 0 1.25-1.25.625.625 0 1 1 1.25 0c.001.69.56 1.249 1.25 1.25a.625.625 0 1 1 0 1.25c-.69.001-1.249.56-1.25 1.25A.625.625 0 0 1 12.5 5Z"
+            />
+          </svg>
+          <span class="sr-only">Switch to light / dark version</span>
+        </button>
+
+        <!-- Profile -->
         <h1 class="font-bold text-4xl text-center md:hidden dark:text-white">
           C<span class="text-teal-600 dark:text-emerald-400">.</span>
         </h1>
@@ -88,6 +128,7 @@
           </div>
         </div>
 
+        <!-- Search -->
         <div id="search">
           <div
             class="flex border-2 dark:border border-gray-200 rounded-md focus-within:ring-2 ring-teal-500 dark:ring-teal-400"
@@ -120,6 +161,7 @@
           </div>
         </div>
 
+        <!-- Menu -->
         <div id="menu" class="flex flex-col items-start space-y-2 text-left">
           <ButtonSideBar
             @buttonClicked="selectComponent"
@@ -204,6 +246,22 @@
   import citations from "@/datas/sagesse_jcvd.json";
 
   const searchQuery = ref("");
+  const isDarkMode = ref(false);
+
+  /*****************************************
+   *           Dark mode toggle             *
+   ******************************************/
+
+  const toggleTheme = () => {
+    isDarkMode.value = !isDarkMode.value;
+    if (isDarkMode.value) {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  };
 
   /*****************************************
    *       Activation des composants       *
@@ -289,6 +347,10 @@
   const isMobile = computed(() => getViewportDimensions().viewportWidth < 768);
 
   onMounted(() => {
+    if (localStorage.getItem("theme") === "dark") {
+      isDarkMode.value = true;
+      document.documentElement.classList.add("dark");
+    }
     updateViewportDimensions();
     window.addEventListener("resize", updateViewportDimensions);
   });
