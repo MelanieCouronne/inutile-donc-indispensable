f4ab6003f76bd83b6d71e23ff0a4b9eee3d09530
Mélanie Couronne <melanie.couronne@gmail.com>
2024-09-10 16:32:24 +0200

    Ajout de meta titre

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import PresentationView from "@/components/layouts/PresentationView.vue";
 import GitHubActivity from "@/components/layouts/GithubActivity.vue";
-import ColorfulRods from "@/components/layouts/ColorfulRods.vue";
-import ElusiveView from "@/components/layouts/ElusiveView.vue";
-import MondrianFrames from "@/components/layouts/MondrianFrames.vue";
 import WelcomeView from "@/views/WelcomeView.vue";
-import ConferenceGallery from "@/components/layouts/ConferenceGallery.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,11 +11,17 @@ const router = createRouter({
       path: "/",
       name: "welcome",
       component: WelcomeView,
+      meta: {
+        title: import.meta.env.VITE_APP_TITLE,
+      },
     },
     {
       path: "/accueil",
       name: "accueil",
       component: HomeView,
+      meta: {
+        title: import.meta.env.VITE_APP_TITLE,
+      },
       children: [
         {
           path: "make-my-point",
@@ -34,42 +36,42 @@ const router = createRouter({
         {
           path: "composant-inutile-2",
           name: "rods",
-          component: ColorfulRods,
+          component: () => import("@/components/layouts/ColorfulRods.vue"),
         },
         {
           path: "composant-inutile-3",
           name: "elusive",
-          component: ElusiveView,
+          component: () => import("@/components/layouts/ElusiveView.vue"),
         },
         {
           path: "composant-inutile-4",
           name: "mondrian",
-          component: MondrianFrames,
+          component: () => import("@/components/layouts/MondrianFrames.vue"),
         },
         {
           path: "composant-inutile-5",
           name: "conferences",
-          component: ConferenceGallery,
+          component: () => import("@/components/layouts/ConferenceGallery.vue"),
         },
       ],
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("@/views/AboutView.vue"),
     },
     {
       path: "/:pathMatch(.*)*",
       name: "NotFound",
       component: () => import("@/views/NotFound.vue"),
-      // meta: {
-      //   title: `${import.meta.env.VITE_APP_TITLE} - 404 Not Found`,
-      // },
+      meta: {
+        title: `${import.meta.env.VITE_APP_TITLE} - 404 Not Found`,
+      },
     },
   ],
+});
+
+router.beforeEach((to) => {
+  // Titre dynamique des pages
+  const defaultTitle =
+    import.meta.env.VITE_APP_TITLE || "Inutile, donc indispensable";
+  const title = to.meta.title || defaultTitle;
+  document.title = title;
 });
 
 export default router;
