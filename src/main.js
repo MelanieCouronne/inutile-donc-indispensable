c0772e4fee9017503329c9ab6f7b624cea1f7cca
Mélanie Couronne <melanie.couronne@gmail.com>
2024-09-11 16:31:26 +0200

    Rollback

--- src/main.js
+++ src/main.js
@@ -6,10 +6,4 @@ import router from "./router";
 
 const app = createApp(App);
 
-// Vérifiez si une redirection est nécessaire (pour la 404)
-const redirect = new URLSearchParams(window.location.search).get("redirect");
-if (redirect) {
-  router.replace(redirect);
-}
-
 app.use(router).mount("#app");
