633611539cf185ae803e4d257262a793621d5475
Mélanie Couronne <melanie.couronne@gmail.com>
2024-09-11 15:46:23 +0200

    dist et env + 404

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,12 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const base =
+  process.env.NODE_ENV === "production" ? process.env.VITE_APP_BASE_URL : "/";
 
 export default defineConfig({
   plugins: [vue()],
-  base:
-    process.env.NODE_ENV === "production"
-      ? import.meta.env.VITE_APP_BASE_URL
-      : "/",
+  base: base,
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
