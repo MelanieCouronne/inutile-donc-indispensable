fb7f7cb2a2895f6239d20e563102f61b98e1705c
Mélanie Couronne <melanie.couronne@gmail.com>
2024-09-11 17:27:45 +0200

    Changement vite config

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,12 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const base =
-  process.env.NODE_ENV === "production" ? process.env.VITE_APP_BASE_URL : "/";
+// const base =
+//   process.env.NODE_ENV === "production" ? process.env.VITE_APP_BASE_URL : "/";
 
 export default defineConfig({
   plugins: [vue()],
-  base: base,
+  base: "/inutile-donc-indispensable/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
