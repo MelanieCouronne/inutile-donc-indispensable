3ffba10afa38e9e03bf513a6b24cc693ff93323e
Mélanie Couronne <melanie.couronne@gmail.com>
2024-09-12 11:42:21 +0200

    Fux bug sur Vite env

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,24 @@
 import { fileURLToPath } from "node:url";
 import path from "path";
 
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const base =
-  process.env.NODE_ENV === "production" ? process.env.VITE_APP_BASE_URL : "/";
 
-export default defineConfig({
-  plugins: [vue()],
-  base: base,
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  console.log("NODE_ENV:", mode);
+  console.log("VITE_APP_BASE_URL:", env.VITE_APP_BASE_URL);
+
+  return {
+    plugins: [vue()],
+    base: mode === "production" ? env.VITE_APP_BASE_URL : "/",
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+  };
 });
