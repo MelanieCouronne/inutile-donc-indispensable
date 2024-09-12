40e7fccdbcb9e080274f8cf7b465bf76c84fcaf9
Mélanie Couronne <melanie.couronne@gmail.com>
2024-09-12 12:28:17 +0200

    Changement font size button

--- src/components/commun/buttons/ButtonSideBar.vue
+++ src/components/commun/buttons/ButtonSideBar.vue
@@ -1,9 +1,9 @@
 <template>
   <button
     @click="emitOnClick"
-    class="w-full text-left text-sm md:text-xs font-medium text-gray-700 dark:text-gray-600 py-3 md:py-2 px-2 hover:bg-teal-500 dark:hover:bg-emerald-400 hover:text-white dark:hover:text-slate-800 rounded-md transition duration-150 ease-in-out cursor-pointer"
+    class="w-full text-left text-gray-700 dark:text-gray-600 py-3 md:py-2 px-2 hover:bg-teal-500 dark:hover:bg-emerald-400 hover:text-white dark:hover:text-slate-800 rounded-md transition duration-150 ease-in-out cursor-pointer"
   >
-    <p class="p-0">
+    <p class="p-0 text-sm md:text-xs font-medium">
       <svg
         class="w-6 h-6 md:w-5 md:h-5 fill-current inline-block"
         fill="currentColor"
