62e45892501dbcda216e144e1fe18248b780ba55
Mélanie Couronne <melanie.couronne@gmail.com>
2024-09-16 16:35:29 +0200

    Clean sideb

--- src/utils/composables/useSidebarToggle.js
+++ src/utils/composables/useSidebarToggle.js
@@ -2,22 +2,18 @@ import { ref } from "vue";
 
 /**
  * Utilise un bus d'événements réactif pour gérer l'état de la sidebar et le chargement global.
- * @ref showNavigation - Un état réactif pour afficher ou masquer le contenu de la sidebar.
- * @ref sidebarEvent - Un bus d'événements réactif pour gérer l'état de la sidebar.
+ * @ref showNavigation - Un état réactif pour afficher ou masquer la sidebar.
  * @function toggleNavigation - Une fonction pour basculer l'état de la sidebar.
  * @returns {Object} - Un objet contenant les états réactifs et la fonction associée.
  */
 
 export function useSideBarToggle() {
   const showNavigation = ref(true);
-  // const sidebarEvent = reactive({ isSidebarVisible: true });
 
   const toggleNavigation = () => {
-    console.log("🚀 ~ showNavigation depart ~ : ", showNavigation.value);
+    // console.log("🚀 ~ showNavigation depart ~ : ", showNavigation.value);
     showNavigation.value = !showNavigation.value;
-    console.log("🚀 ~ showNavigation fin ~ : ", showNavigation.value);
-
-    // sidebarEvent.isSidebarVisible = !sidebarEvent.isSidebarVisible;
+    // console.log("🚀 ~ showNavigation fin ~ : ", showNavigation.value);
   };
 
   return { showNavigation, toggleNavigation };
